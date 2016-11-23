671fc06b04e16a9921b9cd62fe0f02f733225d10
AAlphaDev <arthurcho72@gmail.com>
2016-11-23 22:48:51 +0100

    Modals

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
-//jQuery page scrolling using jQuery Easing plugin
 $(function() {
+	//jQuery page scrolling using jQuery Easing plugin
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
+    });
+
+    $('.to-modal1').data('modal', '#modal1');
+    $('#modal1').hide();
+
+    $('.modal-link').on('click', function(event) {
+        var modal = $(this).data('modal');
+        $(modal).fadeToggle();
     });
 });
 
