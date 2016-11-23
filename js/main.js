5e31276aea426334a29bddc8155612dcadb1fea2
AAlphaDev <arthurcho72@gmail.com>
2016-11-23 21:47:06 +0100

    Purifying

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//jQuery page scrolling using jQuery Easing plugin
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+//Closes the Responsive menu on item click (little screen)
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
