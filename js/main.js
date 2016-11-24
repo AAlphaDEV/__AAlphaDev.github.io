6efd2e7bd11dc45f1e2ef9d368b35c665a9e6970
AAlphaDev <arthurcho72@gmail.com>
2016-11-24 23:10:48 +0100

    Adding popup pages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
+var current_ppage;
+
+/*** Start of scroll disabler ***/
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+function disableScroll() {
+  if (window.addEventListener) // older FF
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll() {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null;
+    window.onwheel = null;
+    window.ontouchmove = null;
+    document.onkeydown = null;
+}
+
+/*** End of scroll disabler ***/
+
 $(function() {
 	//jQuery page scrolling using jQuery Easing plugin
     $('.page-scroll a').bind('click', function(event) {
@@ -8,12 +50,23 @@ $(function() {
         event.preventDefault();
     });
 
-    $('.to-modal1').data('modal', '#modal1');
-    $('#modal1').hide();
+    //Modals
+    $('.to-ppage1').data('ppage', '#ppage1');
+    $('#ppage1').hide();
 
-    $('.modal-link').on('click', function(event) {
-        var modal = $(this).data('modal');
-        $(modal).fadeToggle();
+    $('.ppage-link').on('click', function(event) {
+        current_ppage = $(this).data('ppage');
+
+        $(current_ppage).fadeToggle();
+        disableScroll();
+
+        return false;
+    });
+    $('.close-ppage').on('click', function(event) {
+        $(current_ppage).fadeToggle();
+        enableScroll();
+
+        return false;
     });
 });
 
